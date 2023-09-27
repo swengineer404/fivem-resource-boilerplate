863db14779d29a3e6728de538d56d31b7e099edb
Lync <swengineer404@gmail.com>
2023-09-27 17:24:48 -0400

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version "cerulean"
+game "gta5"
+
+client_scripts {
+    "client/**"
+}
+
+server_scripts {
+    "server/**"
+}
+
+shared_scripts {
+    "shared/**"
+}
+
+ui_page "html/main.html"
+
+files {
+    "html/**",
+}
+
+lua54 "yes"
