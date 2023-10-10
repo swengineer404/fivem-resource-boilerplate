ab3059db45fa9db56f7b046b33f0999267a4a0bb
Lync <swengineer404@gmail.com>
2023-10-10 14:07:43 -0400

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 $(async function () {
   Events.init()
 
+  $(document).on('click', 'a', function (e) {
+    e.preventDefault()
+  })
+
+  $(document).on('contextmenu', function (e) {
+    return false
+  })
+
+  $(document).on('submit', function (e) {
+    e.preventDefault()
+  })
+
   Utils.fetch(`https://${window.GetParentResourceName()}/onLoad`).catch((e) => {})
 })
